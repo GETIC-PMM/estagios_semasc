23a9f9a0d44cf014227f10b26b0b1b6d91151568
Giovanni Martins <gmgit10@gmail.com>
2025-10-06 14:50:44 -0300

    config puma_production

--- config/puma_production.rb
+++ config/puma_production.rb
@@ -16,14 +16,14 @@ rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/sockets/estagiarios.sock"
-bind "tcp://0.0.0.0:3000"
-#bind 'ssl://0.0.0.0:3001?key=/etc/letsencrypt/live/estagiarios.com.br/privkey.pem&cert=/etc/letsencrypt/live/estagiarios.com.br/cert.pem'
+bind "unix://#{shared_dir}/sockets/estagios_semasc.sock"
+bind "tcp://0.0.0.0:3077"
+#bind 'ssl://0.0.0.0:3001?key=/etc/letsencrypt/live/estagios_semasc.com.br/privkey.pem&cert=/etc/letsencrypt/live/estagios_semasc.com.br/cert.pem'
 
 # Logging
-stdout_redirect "#{shared_dir}/log/estagiarios.stdout.log", "#{shared_dir}/log/estagiarios.stderr.log", true
+stdout_redirect "#{shared_dir}/log/estagios_semasc.stdout.log", "#{shared_dir}/log/estagios_semasc.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/estagiarios.pid"
-state_path "#{shared_dir}/pids/estagiarios.state"
+pidfile "#{shared_dir}/pids/estagios_semasc.pid"
+state_path "#{shared_dir}/pids/estagios_semasc.state"
 activate_control_app
